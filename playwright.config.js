eecb563932e942cbae472e0fcfca271438e1e04b
XiaoDie <luanle.kt7@gmail.com>
2025-02-18 16:33:06 +0700

    Update CI workflow to continue on error for report zipping and email notification; remove WebKit configuration from Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -18,10 +18,6 @@ module.exports = defineConfig({
       name: "firefox", // Chạy trên Firefox
       use: { browserName: "firefox" },
     },
-    {
-      name: "webkit", // Chạy trên WebKit
-      use: { browserName: "webkit" },
-    },
   ],
   workers: 3, // Số worker chạy song song
   reporter: [["html", { open: "never" }]],
